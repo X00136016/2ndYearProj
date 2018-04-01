4ec6f53bf9c0a0791459d82ee706557e628db55d
Posttwo <post159510@gmail.com>
2018-04-01 16:31:07 +0100

    Setup basic tempalte

--- Y2Project/jobs/views.py
+++ Y2Project/jobs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the jobs index.")
+    template = loader.get_template('jobs/index.html')
+    return HttpResponse(template.render(None, request))
