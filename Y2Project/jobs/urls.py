4232c6b759abb1532c50a9077ce284afac244a23
Posttwo <post159510@gmail.com>
2018-04-06 11:33:57 +0100

    Move all tempaltes into views

--- Y2Project/jobs/urls.py
+++ Y2Project/jobs/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('articles', views.articles, name='articles'),
+    path('article/<article>', views.article, name='article'),
+    path('jobs', views.jobs, name='jobs'),
 ]
--- Y2Project/jobs/views.py
+++ Y2Project/jobs/views.py
@@ -7,3 +7,15 @@ from django.template import loader
 def index(request):
     template = loader.get_template('jobs/index.html')
     return HttpResponse(template.render(None, request))
+
+def articles(request):
+    template = loader.get_template('jobs/articles.html')
+    return HttpResponse(template.render(None, request))
+
+def article(request, article):
+    template = loader.get_template('jobs/article.html')
+    return HttpResponse(template.render(None, request))
+
+def jobs(request):
+    template = loader.get_template('jobs/jobs.html')
+    return HttpResponse(template.render(None, request))
